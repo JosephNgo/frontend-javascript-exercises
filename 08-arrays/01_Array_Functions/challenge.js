f3f36f01e49dfd9ee51864a878c3d887143b26aa
Joe Ngo <joengo83@gmail.com>
2014-09-11 04:48:04 +0000

    completed 08-Arrays; 09-objects incomplete

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,25 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(userArray){
+var newArray = [];
+userArray.push(1);
+  for (var i = 0; i < userArray.length; i++) {
+    newArray.unshift(userArray[i]);
+  };
+  return  newArray;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere =  function(userArray){
+var newArray = "";
 
-module.exports.arrayQuantityPlusOne = undefined;
+  for (var i = 0; i < userArray.length; i++) {
+    newArray += '+' + userArray[i];
+
+  };
+
+var stringLength = newArray.length;
+return  newArray.substring(1, stringLength);
+};
+
+module.exports.arrayQuantityPlusOne = function(userArray){
+arrayLength = userArray.length+1;
+return arrayLength;
+};
