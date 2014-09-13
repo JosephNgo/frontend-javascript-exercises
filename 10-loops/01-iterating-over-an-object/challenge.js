d83addbdaa7b5de799d0670255d22973f9a48ad6
Joe Ngo <joengo83@gmail.com>
2014-09-13 21:11:00 +0000

    Completed 10-Loops 01-iterating over an object

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,28 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(myHash){
+  var myObject = myHash;
+  var myKeys = [];
+  for (var property in myObject) {
+    myKeys += property;
+  }
+ return myKeys;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(myHash){
+  var myObject = myHash;
+  var myValues = [];
+  for (var property in myObject) {
+    myValues += myObject[property];
+  }
+  return myValues;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(myHash) {
+  var myObject = myHash;
+  var myKeys = [];
+  var myValues = [];
+  var myArray = [];
+  for (var property in myObject) {
+    myArray += property + " is " + myObject[property];
+  }
+  return myArray;
+};
