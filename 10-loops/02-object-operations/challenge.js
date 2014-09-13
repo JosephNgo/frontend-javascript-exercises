94c1f194f6aefd1481ccd0bd8142e9408401431a
Joe Ngo <joengo83@gmail.com>
2014-09-13 22:16:13 +0000

    completed 10-loops/02-object-operations

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,37 @@
-module.exports.copy = undefined;
+module.exports.copy = function(myHash) {
+  var myObjectCopy = {};  
+  for (var property in myHash) {
+    myObjectCopy[property] = myHash[property];
+  }
+  return myObjectCopy;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(hash1, hash2) {
+  var myObject = hash1;
+  for (var property in hash2) {
+    myObject[property] = hash2[property];
+  }
+  return myObject;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(myHash, compareKeyArray){
+  var myObject = myHash;
+  var myKeys = [];
+  var compareKeys = compareKeyArray;
+  var compareKeysLength = compareKeys.length;
+  for (var property in myObject) {
+    myKeys.push(property);
+  }
+  if ((compareKeysLength == 0) || (myKeys.length == compareKeysLength)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+  while (i--) {
+    if (myKeys[i] !== compareKeys[i]) return false;
+  }
+  return true;
+};
+
+
