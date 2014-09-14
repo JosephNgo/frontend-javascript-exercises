319e35a5a13897ade420a5f2b1665b155e4a410a
Joe Ngo <joengo83@gmail.com>
2014-09-14 06:31:11 +0000

    08-arrays/02_Grocery_list complete

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,16 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(newItem, originalArray){
+  var newArray = originalArray;
+  if (originalArray[0] == newItem) {
+    return newArray
+  }
+  else {
+    newArray.push(newItem);
+    return newArray;
+  }
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(myArray) {
+  var newArray = myArray;
+  newArray.sort().reverse();
+  return newArray;
+};
