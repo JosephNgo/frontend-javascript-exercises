929ce45443f60e93b7da34c845913645d215f4b8
Joe Ngo <joengo83@gmail.com>
2014-09-14 06:13:14 +0000

    10-loops/03-while-loops complete

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,17 @@
-module.exports.stream = undefined;
+module.exports.stream = function (conditionalFn, actionFn) {
+  while (conditionalFn() > 0) {
+    actionFn();
+  }
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(myArray){
+  var sum = 0;
+  var counter = 0;
+
+  while (counter < myArray.length) {
+    sum += myArray[counter];
+    counter++;
+  }
+  return sum;
+};
 
