824201091d2d3aed56684c55e797f38d7f40977a
Joe Ngo <joengo83@gmail.com>
2014-09-14 06:44:09 +0000

    Completed 09-objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,51 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+  var newCourse = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
 
-module.exports.addProperty = undefined;
+  return newCourse;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(teacher, favoriteStudent, studentName){
+  teacher[favoriteStudent] = studentName;
+  return teacher;
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter){
+  var letterStr = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+
+  return letterStr;
+};
+
+//do not use if statement. 
+module.exports.canIGet = function(item, money){
+  if ((item == "MacBook Air") && (money > 998)){
+    return true;
+  }
+  else if ((item == "MacBook Air") && (money <= 998)) {
+    return false;
+  }
+  else if ((item == "MacBook Pro") && (money > 1298)){
+    return true;
+  }
+  else if ((item == "MacBook Pro") && (money <= 1298)) {
+    return false;
+  }
+  else if ((item == "Mac Pro") && (money > 2498)){
+    return true;
+  }
+  else if ((item == "Mac Pro") && (money <= 2498)) {
+    return false;
+  }
+  else if ((item == "Apple Sticker") && (money > 0)){
+    return true;
+  }
+  else if ((item == "Apple Sticker") && (money <= 0)) {
+    return false;
+  }
+  else {
+    return false;
+  }
+};
