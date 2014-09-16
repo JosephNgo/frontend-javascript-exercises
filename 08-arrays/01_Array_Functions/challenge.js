24f970774747bdbd5a463901a63b7f6eee638a0d
Joe Ngo <joengo83@gmail.com>
2014-09-16 01:38:22 +0000

    refactored Arrays and Objects files

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,24 +1,12 @@
 module.exports.reversePlusOne = function(userArray){
-  var newArray = [];
-  userArray.push(1);
-  for (var i = 0; i < userArray.length; i++) {
-    newArray.unshift(userArray[i]);
-  };
-  return  newArray;
+  userArray.reverse().unshift(1);
+  return userArray;
 };
 
 module.exports.plusesEverywhere =  function(userArray){
-  var newArray = "";
-
-  for (var i = 0; i < userArray.length; i++) {
-    newArray += '+' + userArray[i];
-
-  };
-
-  return  newArray.substring(1);
+  return userArray.join('+');
 };
 
 module.exports.arrayQuantityPlusOne = function(userArray){
-  arrayLength = userArray.length+1;
-  return arrayLength;
+  return userArray.length+1;
 };
--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -21,31 +21,41 @@ module.exports.formLetter = function(letter){
 
 //do not use if statement. 
 module.exports.canIGet = function(item, money){
-  if ((item == "MacBook Air") && (money > 998)){
-    return true;
-  }
-  else if ((item == "MacBook Air") && (money <= 998)) {
-    return false;
-  }
-  else if ((item == "MacBook Pro") && (money > 1298)){
-    return true;
-  }
-  else if ((item == "MacBook Pro") && (money <= 1298)) {
-    return false;
-  }
-  else if ((item == "Mac Pro") && (money > 2498)){
-    return true;
-  }
-  else if ((item == "Mac Pro") && (money <= 2498)) {
-    return false;
-  }
-  else if ((item == "Apple Sticker") && (money > 0)){
-    return true;
-  }
-  else if ((item == "Apple Sticker") && (money <= 0)) {
-    return false;
-  }
-  else {
-    return false;
-  }
+  // if ((item == "MacBook Air") && (money > 998)){
+  //   return true;
+  // }
+  // else if ((item == "MacBook Air") && (money <= 998)) {
+  //   return false;
+  // }
+  // else if ((item == "MacBook Pro") && (money > 1298)){
+  //   return true;
+  // }
+  // else if ((item == "MacBook Pro") && (money <= 1298)) {
+  //   return false;
+  // }
+  // else if ((item == "Mac Pro") && (money > 2498)){
+  //   return true;
+  // }
+  // else if ((item == "Mac Pro") && (money <= 2498)) {
+  //   return false;
+  // }
+  // else if ((item == "Apple Sticker") && (money > 0)){
+  //   return true;
+  // }
+  // else if ((item == "Apple Sticker") && (money <= 0)) {
+  //   return false;
+  // }
+  // else {
+  //   return false;
+  // }
+
+  var priceList = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  };
+
+  return priceList[item]<=money;
+
 };
